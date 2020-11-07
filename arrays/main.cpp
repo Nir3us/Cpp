554025c280bf0c40d23a56904904cb8a1176be7a
Nir3us <32523762+Nir3us@users.noreply.github.com>
2020-11-07 23:08:48 +0100

    Add files via upload

---
+++ arrays/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+
+int main()
+{
+    int array[5]{ 0, 1, 2, 3, 4 };
+    std::cout << array[0] << '\n';
+    std::cout << array[1] << '\n';
+    std::cout << array[2] << '\n';
+    std::cout << array[3] << '\n';
+    std::cout << array[4] << '\n';
+
+
+    return 0;
+}
