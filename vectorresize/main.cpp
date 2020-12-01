b9494f455168423e1f8e4e6c642bc7cf4af5656a
Nir3us <32523762+Nir3us@users.noreply.github.com>
2020-12-01 21:29:21 +0100

    Add files via upload

---
+++ vectorresize/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+void printStack(const std::vector<int> &stack)
+{
+	for (auto element : stack)
+		std::cout << element << ' ';
+	std::cout << "(cap " << stack.capacity() << " length " << stack.size() << ")\n";
+}
+
+int main()
+{
+	std::vector<int> stack{};
+
+	printStack(stack);
+
+	stack.push_back(5);
+	printStack(stack);
+
+	stack.push_back(3);
+	printStack(stack);
+
+	stack.push_back(2);
+	printStack(stack);
+
+	std::cout << "top: " << stack.back() << '\n';
+
+	stack.pop_back();
+	printStack(stack);
+
+	stack.pop_back();
+	printStack(stack);
+
+	stack.pop_back();
+	printStack(stack);
+
+	return 0;
+}
