13e54e7a0127eafa55151f226c28bc28ca8797cb
Nir3us <32523762+Nir3us@users.noreply.github.com>
2021-02-10 23:22:50 +0100

    Add files via upload

---
+++ accessSpecs/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class Base
+{
+protected:
+    int m_value;
+
+public:
+    Base(int value)
+        : m_value(value)
+    {
+    }
+
+    void identify() { std::cout << "I am a Base\n"; }
+};
+
+
+
+class Derived: public Base
+{
+public:
+    Derived(int value)
+        :Base(value)
+    {
+    }
+
+    int getValue() { return m_value; }
+};
+
+int main()
+{
+    Derived derived(5);
+    std::cout << "derived has value " << derived.getValue() << '\n';
+
+    return 0;
+}
