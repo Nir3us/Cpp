1b76cb3f5952556d0ce94d6cdbd93e01bcf90419
Nir3us <32523762+Nir3us@users.noreply.github.com>
2021-02-27 17:46:31 +0100

    Add files via upload

---
+++ covariantReturnTypes/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class Base
+{
+public:
+	virtual Base* getThis() { std::cout << "called Base::getThis()\n"; return this; }
+	void printType() { std::cout << "returned a Base\n"; }
+};
+
+class Derived : public Base
+{
+public:
+	Derived* getThis() override { std::cout << "called Derived::getThis()\n";  return this; }
+	void printType() { std::cout << "returned a Derived\n"; }
+};
+
+int main()
+{
+	Derived d{};
+	Base* b{ &d };
+	d.getThis()->printType();
+	b->getThis()->printType();
+
+	return 0;
+}
