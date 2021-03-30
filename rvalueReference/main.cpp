83ada86407e43c702b8b5f70e7c6126bae17ee62
Nir3us <32523762+Nir3us@users.noreply.github.com>
2021-03-30 23:18:33 +0200

    Add files via upload

---
+++ rvalueReference/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class Fraction
+{
+private:
+	int m_numerator;
+	int m_denominator;
+
+public:
+	Fraction(int numerator = 0, int denominator = 1) :
+		m_numerator{ numerator }, m_denominator{ denominator }
+	{
+	}
+
+	friend std::ostream& operator<<(std::ostream& out, const Fraction &f1)
+	{
+		out << f1.m_numerator << '/' << f1.m_denominator;
+		return out;
+	}
+};
+
+int main()
+{
+	auto &&rref{ Fraction{ 3, 5 } };
+
+
+    std::cout << rref << '\n';
+
+	return 0;
+}
