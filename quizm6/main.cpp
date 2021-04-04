9dee8f8c643cb39e4720a46eef722f0e12fd1877
Nir3us <32523762+Nir3us@users.noreply.github.com>
2021-04-04 23:15:06 +0200

    Add files via upload

---
+++ quizm6/main.cpp
@@ -0,0 +1,37 @@
+#include <memory>
+#include <iostream>
+
+class Fraction
+{
+private:
+	int m_numerator{ 0 };
+	int m_denominator{ 1 };
+
+public:
+	Fraction(int numerator = 0, int denominator = 1) :
+		m_numerator{ numerator }, m_denominator{ denominator }
+	{
+	}
+
+	friend std::ostream& operator<<(std::ostream& out, const Fraction &f1)
+	{
+		out << f1.m_numerator << '/' << f1.m_denominator;
+		return out;
+	}
+};
+
+
+void printFraction(const Fraction* ptr)
+{
+	if (ptr)
+		std::cout << *ptr << '\n';
+}
+
+int main()
+{
+	auto ptr{ std::make_unique<Fraction>(3, 5) };
+
+	printFraction(ptr.get());
+
+	return 0;
+}
