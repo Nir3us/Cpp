8c05bf3daeb806a3d7c276b96b378903da0e446e
Nir3us <32523762+Nir3us@users.noreply.github.com>
2021-04-12 22:47:34 +0200

    Add files via upload

---
+++ stltryout/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+int main()
+{
+	std::map<int, std::string> mymap;
+	mymap.insert(std::make_pair(4, "apple"));
+	mymap.insert(std::make_pair(2, "orange"));
+	mymap.insert(std::make_pair(1, "banana"));
+	mymap.insert(std::make_pair(3, "grapes"));
+	mymap.insert(std::make_pair(6, "mango"));
+	mymap.insert(std::make_pair(5, "peach"));
+
+	auto it{ mymap.cbegin() };
+	while (it != mymap.cend())
+	{
+		std::cout << it->first << "=" << it->second << " ";
+		++it;
+	}
+
+	std::cout << '\n';
+}
