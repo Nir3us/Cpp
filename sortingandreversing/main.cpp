a1ab9d322d5b2abe6aeb8a5b18ad882035524c8d
Nir3us <32523762+Nir3us@users.noreply.github.com>
2021-04-13 21:07:43 +0200

    Add files via upload

---
+++ sortingandreversing/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::vector<int> vect{ 7, -3, 6, 2, -5, 0, 4 };
+
+    std::sort(vect.begin(), vect.end());
+
+    for (int i : vect)
+    {
+        std::cout << i << ' ';
+    }
+
+    std::cout << '\n';
+
+    std::reverse(vect.begin(), vect.end());
+
+    for (int i : vect)
+    {
+        std::cout << i << ' ';
+    }
+
+    std::cout << '\n';
+
+    return 0;
+}
