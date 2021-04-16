05638a56af18f645ecd067d5480b692258be0358
Nir3us <32523762+Nir3us@users.noreply.github.com>
2021-04-16 12:08:46 +0200

    Add files via upload

---
+++ swapingstrings/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string sStr1("red");
+    string sStr2("blue");
+
+    cout << sStr1 << " " << sStr2 << endl;
+    swap(sStr1, sStr2);
+    cout << sStr1 << " " << sStr2 << endl;
+    sStr1.swap(sStr2);
+    cout << sStr1 << " " << sStr2 << endl;
+    return 0;
+}
