2a5e7ddb3e8d990b110df7b9bf52cb1967aa9dfe
Nir3us <32523762+Nir3us@users.noreply.github.com>
2021-04-22 23:14:54 +0200

    Add files via upload

---
+++ numericvalidation/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <limits>
+
+int main()
+{
+    int age{};
+
+    while (true)
+    {
+        std::cout << "Enter your age: ";
+        std::cin >> age;
+
+        if (std::cin.fail()) // no extraction took place
+        {
+            std::cin.clear(); // reset the state bits back to goodbit so we can use ignore()
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // clear out the bad input from the stream
+            continue; // try again
+        }
+
+        if (age <= 0) // make sure age is positive
+            continue;
+
+        break;
+    }
+
+    std::cout << "You entered: " << age << '\n';
+}
